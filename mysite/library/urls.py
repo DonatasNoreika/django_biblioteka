367f954fa071855162684ea52cfad9237a14949f
Donatas Noreika <donoras@gmail.com>
2022-03-31 18:36:14 +0300

    9: added registration

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('search/', views.search, name='search'),
-    path('userbooks/', views.UserBookListView.as_view(), name='userbooks')
+    path('userbooks/', views.UserBookListView.as_view(), name='userbooks'),
+    path('register/', views.register, name='register'),
 ]
